cfeddc7da1903d2e0e93c18c41c64936ac5df507
damian.ionic <damian@ionic.io>
2023-11-06 07:41:22 -0800

    feat: add cordova plugin branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
@@ -7,6 +7,22 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    const MopinionSDK = (window as any).MopinionSDK;
+    MopinionSDK.load(
+      'x0W6z2F8gnJ6TdefIak95Wk4Ot4qNAz5dzxAnyuD',
+      true,
+      function (result: any) {
+        console.log('OK');
+
+      },
+      function (err: any) {
+        console.log('Error');
+
+      }
+    );
+  }
 }
